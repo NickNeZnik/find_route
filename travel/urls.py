63167f31f0b841c5266513f6e60744ee0616f74c
NickNeZnik <nicolyaskrobot@gmail.com>
2022-02-15 23:11:00 +0300

    Pagination

--- travel/urls.py
+++ travel/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from travel.views import home
+from django.urls import path, include
+from travel.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('cities/', include(('cities.urls', 'cities'))),
+    path('', home, name='home'),
+    path('about/', about)
 ]
